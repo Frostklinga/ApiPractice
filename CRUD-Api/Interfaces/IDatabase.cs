ff8d8fc9a2c10283c084b4e09f2c72a427c84467
Daniel Frostklinga <daniel@frostklinga.se>
2022-03-21 23:36:56 +0100

    Added a Interface for the database to implement.

---
+++ CRUD-Api/Interfaces/IDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CRUD_Api.Interfaces
+{
+    public interface IDatabase
+    {
+        List<string> Get();
+        bool Remove();
+        bool Replace(string existingTool, string newTool);
+        bool Delete(string existingTool);
+        bool Update(string existingTool, string newTool);
+        string GetTool(string tool);
+    }
+}
