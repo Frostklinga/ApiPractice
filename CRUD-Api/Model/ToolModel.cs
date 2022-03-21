9d4d5e11b62887e03781565cb1f88ab86f5fcfb0
Daniel Frostklinga <daniel@frostklinga.se>
2022-03-21 23:39:46 +0100

    Added a model for tools

---
+++ CRUD-Api/Model/ToolModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_Api.Model
+{
+    public class ToolModel
+    {
+        public int Id { get; private set; }
+        public string Tool { get; private set; }
+        public DateTime TimeOfPurchase { get; private set; }
+        public string Owner { get; private set; }
+    }
+}
